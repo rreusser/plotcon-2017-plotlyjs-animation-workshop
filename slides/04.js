f9120a0726766e8bc8551ba8fe95428e194db4f1
Ricky Reusser <rsreusser@gmail.com>
2017-05-04 16:51:56 -0700

    Drop slide

--- slides/04.js
+++ slides/04.js
@@ -1,15 +1,35 @@
-Plotly.plot('graph', [{
-  x: [1, 2, 3],
-  y: [2, 4, 3],
-  line: {simplify: false}
-}]).then(function () {
-  return Plotly.animate('graph', [{
-    data: [{
-      y: [3, 4, 2]
-    }]
-  }, {
-    data: [{
-      y: [4, 2, 3]
-    }]
-  }]);
+var frame1 = {
+  name: 'frame1',
+  data: [{
+    y: [3, 4, 2]
+  }]
+};
+
+var frame2 = {
+  name: 'frame2',
+  data: [{
+    y: [4, 2, 3]
+  }]
+};
+
+Plotly.plot('graph', {
+  data: [{
+    x: [1, 2, 3],
+    y: [2, 4, 3],
+    line: {simplify: false}
+  }],
+  frames: [frame1, frame2]
+}).then(function () {
+  return Plotly.animate('graph',
+    ['frame1', 'frame2'],
+    {
+      transition: {
+        duration: 1000
+      },
+      frame: {
+        duration: 2000,
+        redraw: false
+      }
+    }
+  );
 });
